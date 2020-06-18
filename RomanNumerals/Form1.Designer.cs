1070dc48abe5e876fddfee049c4b50a1ac6c32f1
M <M@DESKTOP-K0HAN3Q>
2020-06-18 18:48:40 +0200

    Add project files.

---
+++ RomanNumerals/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace RomanNumerals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonI = new System.Windows.Forms.Button();
+            this.buttonV = new System.Windows.Forms.Button();
+            this.buttonX = new System.Windows.Forms.Button();
+            this.buttonL = new System.Windows.Forms.Button();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.buttonD = new System.Windows.Forms.Button();
+            this.buttonM = new System.Windows.Forms.Button();
+            this.ResultBoxOne = new System.Windows.Forms.TextBox();
+            this.Convert = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonI
+            // 
+            this.buttonI.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonI.Location = new System.Drawing.Point(12, 150);
+            this.buttonI.Name = "buttonI";
+            this.buttonI.Size = new System.Drawing.Size(50, 50);
+            this.buttonI.TabIndex = 0;
+            this.buttonI.Text = "I";
+            this.buttonI.UseVisualStyleBackColor = true;
+            this.buttonI.Click += new System.EventHandler(this.NumeralClick);
+            // 
+            // buttonV
+            // 
+            this.buttonV.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonV.Location = new System.Drawing.Point(68, 150);
+            this.buttonV.Name = "buttonV";
+            this.buttonV.Size = new System.Drawing.Size(50, 50);
+            this.buttonV.TabIndex = 1;
+            this.buttonV.Text = "V";
+            this.buttonV.UseVisualStyleBackColor = true;
+            this.buttonV.Click += new System.EventHandler(this.NumeralClick);
+            // 
+            // buttonX
+            // 
+            this.buttonX.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonX.Location = new System.Drawing.Point(124, 150);
+            this.buttonX.Name = "buttonX";
+            this.buttonX.Size = new System.Drawing.Size(50, 50);
+            this.buttonX.TabIndex = 2;
+            this.buttonX.Text = "X";
+            this.buttonX.UseVisualStyleBackColor = true;
+            this.buttonX.Click += new System.EventHandler(this.NumeralClick);
+            // 
+            // buttonL
+            // 
+            this.buttonL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonL.Location = new System.Drawing.Point(180, 150);
+            this.buttonL.Name = "buttonL";
+            this.buttonL.Size = new System.Drawing.Size(50, 50);
+            this.buttonL.TabIndex = 3;
+            this.buttonL.Text = "L";
+            this.buttonL.UseVisualStyleBackColor = true;
+            this.buttonL.Click += new System.EventHandler(this.NumeralClick);
+            // 
+            // buttonC
+            // 
+            this.buttonC.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonC.Location = new System.Drawing.Point(12, 206);
+            this.buttonC.Name = "buttonC";
+            this.buttonC.Size = new System.Drawing.Size(50, 50);
+            this.buttonC.TabIndex = 4;
+            this.buttonC.Text = "C";
+            this.buttonC.UseVisualStyleBackColor = true;
+            this.buttonC.Click += new System.EventHandler(this.NumeralClick);
+            // 
+            // buttonD
+            // 
+            this.buttonD.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonD.Location = new System.Drawing.Point(68, 206);
+            this.buttonD.Name = "buttonD";
+            this.buttonD.Size = new System.Drawing.Size(50, 50);
+            this.buttonD.TabIndex = 5;
+            this.buttonD.Text = "D";
+            this.buttonD.UseVisualStyleBackColor = true;
+            this.buttonD.Click += new System.EventHandler(this.NumeralClick);
+            // 
+            // buttonM
+            // 
+            this.buttonM.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonM.Location = new System.Drawing.Point(124, 206);
+            this.buttonM.Name = "buttonM";
+            this.buttonM.Size = new System.Drawing.Size(50, 50);
+            this.buttonM.TabIndex = 6;
+            this.buttonM.Text = "M";
+            this.buttonM.UseVisualStyleBackColor = true;
+            this.buttonM.Click += new System.EventHandler(this.NumeralClick);
+            // 
+            // ResultBoxOne
+            // 
+            this.ResultBoxOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultBoxOne.Location = new System.Drawing.Point(12, 106);
+            this.ResultBoxOne.Name = "ResultBoxOne";
+            this.ResultBoxOne.Size = new System.Drawing.Size(776, 38);
+            this.ResultBoxOne.TabIndex = 7;
+            this.ResultBoxOne.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(396, 163);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(75, 23);
+            this.Convert.TabIndex = 8;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.ConvertNumeralsToInt);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(396, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ResvereTest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.ResultBoxOne);
+            this.Controls.Add(this.buttonM);
+            this.Controls.Add(this.buttonD);
+            this.Controls.Add(this.buttonC);
+            this.Controls.Add(this.buttonL);
+            this.Controls.Add(this.buttonX);
+            this.Controls.Add(this.buttonV);
+            this.Controls.Add(this.buttonI);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonI;
+        private System.Windows.Forms.Button buttonV;
+        private System.Windows.Forms.Button buttonX;
+        private System.Windows.Forms.Button buttonL;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button buttonD;
+        private System.Windows.Forms.Button buttonM;
+        private System.Windows.Forms.TextBox ResultBoxOne;
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RomanNumerals/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RomanNumerals
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, int> roman_numerals = new Dictionary<string, int>()
+        {
+            {"I",1},
+            {"V",5},
+            {"X",10},
+            {"L",50},
+            {"C",100},
+            {"D",500},
+            {"M",1000}
+        };
+        
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NumeralClick(object sender, EventArgs e)
+        {
+            Button bnum = (Button)sender;
+
+            ResultBoxOne.Text = ResultBoxOne.Text + bnum.Text;
+        }
+
+        public int NumeralsToInt(string numerals)
+        {
+            int number_int = 0;
+            char[] numerals_arr = numerals.ToCharArray();
+            if (numerals_arr.Length == 1)
+            {
+               
+                return roman_numerals[numerals_arr[0].ToString()];
+            }
+            else
+            {
+                for (int i = 0; i < numerals_arr.Length; i++)
+                {
+                    if (i == (numerals_arr.Length - 1))
+                    {
+                        number_int += roman_numerals[numerals_arr[i].ToString()];
+                    }
+                    else if(roman_numerals[numerals_arr[i].ToString()] < roman_numerals[numerals_arr[(i + 1)].ToString()])
+                    {
+                        number_int -= roman_numerals[numerals_arr[i].ToString()];
+                    }
+                    else
+                    {
+                        number_int += roman_numerals[numerals_arr[i].ToString()];
+                    }
+                }
+                return number_int;
+            }
+            
+
+            return 1;
+
+         }
+
+        public string IntToNumerals(int number)
+        {
+            string x = "";
+            
+            while (number != 0)
+            {
+                ResultBoxOne.Text = number.ToString();
+                if (number>= 1000)
+                {
+                    number -= 1000;
+                    x += "M";
+                }
+                else if (number >= 900 && number < 1000)
+                {
+                    number -= 900;
+                    x += "CM";
+                }
+                else if (number >= 500 && number < 900)
+                {
+                    number -= 500;
+                    x += "D";
+                }
+                else if (number >= 400 && number < 500)
+                {
+                    number -= 400;
+                    x += "CD";
+                }
+                else if (number >= 100 && number < 400)
+                {
+                    number -= 100;
+                    x += "C";
+                }
+                else if (number >= 90 && number < 100)
+                {
+                    number -= 90;
+                    x += "XC";
+                }
+                else if (number >= 50 && number < 90)
+                {
+                    number -= 50;
+                    x += "L";
+                }
+                else if (number >= 40 && number < 50)
+                {
+                    number -= 40;
+                    x += "XL";
+                }
+                else if (number >= 10 && number < 40)
+                {
+                    number -= 10;
+                    x += "X";
+                }
+                else if (number >= 9 && number < 10)
+                {
+                    number -= 9;
+                    x += "IX";
+                }
+                else if (number >= 5 && number < 9)
+                {
+                    number -= 5;
+                    x += "V";
+                }
+                else if (number >= 4 && number < 5)
+                {
+                    number -= 4;
+                    x += "IV";
+                }
+                else
+                {
+                    number -= 1;
+                    x += "I";
+                    ResultBoxOne.Text = "help";
+                }
+                
+
+            }
+            
+            return x;
+        }
+
+        private void ConvertNumeralsToInt(object sender, EventArgs e)
+        {
+            int x;
+            x = NumeralsToInt(ResultBoxOne.Text);
+            //ResultBoxOne.Text = IntToNumerals(x);
+            ResultBoxOne.Text = IntToNumerals(999);
+        }
+
+        private void ResvereTest(object sender, EventArgs e)
+        {
+            string y;
+            int x = Int32.Parse(ResultBoxOne.Text);
+            y = IntToNumerals(x);
+            ResultBoxOne.Text = y;
+        }
+    }
+}
